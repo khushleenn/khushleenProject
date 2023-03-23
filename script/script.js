2287b51dc340f5b89114db3a787c6465b4f6ae7a
Khushleenn <khush.leen.p@gmail.com>
2023-03-23 12:23:45 -0400

    first commit

---
+++ script/script.js
@@ -0,0 +1,16 @@
+function myFunction(){
+    alert("'Clouds'- by Neil Chand");
+}
+
+function myFunction1(){
+    alert("'Sunset'- by Dave Watt");
+}
+
+function myFunction2(){
+    alert("'Beach'- by Nora Hailey");
+}
+
+function myFunction3(){
+    alert("'Island'- by Sneha Duggal");
+}
+
